0b0a377cd6f937deac8217500ce90220727ff76e
LuNu <tranquangluong06@gmail.com>
2026-07-10 23:05:39 +0700

    first commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import './assets/style.css' // File chứa cấu hình TailwindCSS
 import App from './App.vue'
 import router from './router' // Nạp cấu hình Router định tuyến
+import './assets/style.css'
 
 const app = createApp(App)
 
